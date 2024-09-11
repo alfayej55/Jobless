4e5bdc2fa4c5c231fb44c2168a13b1e7a6400c44
alfayej55 <mdalfayej017fa@gmail.com>
2024-09-11 18:17:53 +0600

    tab bar add

---
+++ lib/views/screen/Home/friend_suggested.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../Widget/friend_request_card.dart';
+
+class FriendSuggested extends StatelessWidget {
+  const FriendSuggested({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        itemCount: 5,
+        padding: EdgeInsets.symmetric(vertical:10.h),
+        shrinkWrap: true,
+        primary: false,
+        itemBuilder: (context,index){
+      return   Padding(
+        padding: EdgeInsets.symmetric(vertical:10.h),
+        child: FriendRequestCard(),
+      );
+    });
+  }
+}
--- lib/views/screen/Home/search_screen.dart
+++ lib/views/screen/Home/search_screen.dart
@@ -1,68 +1,92 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:get/get.dart';
 import 'package:jobless/utils/app_string.dart';
 import 'package:jobless/utils/style.dart';
+import 'package:jobless/views/screen/Home/friend_suggested.dart';
+import 'package:jobless/views/screen/Home/group_suggested.dart';
 
+import '../../../utils/app_colors.dart';
 import '../../../utils/app_icons.dart';
 import '../../base/custom_text_field.dart';
 import '../Widget/friend_request_card.dart';
 
 class SearchScreen extends StatelessWidget {
-   SearchScreen({super.key});
+  SearchScreen({super.key});
 
-  TextEditingController _searchCtrl=TextEditingController();
-
+  final TextEditingController _searchCtrl = TextEditingController();
+  final List<String> tabbarList = ["Friends", "Group"];
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        leading: InkWell(
-          onTap: (){
-            Get.back();
-          },
-            child: Icon(Icons.arrow_back_ios)),
-        backgroundColor: Colors.transparent,
-      ),
-      body: Padding(
-        padding:  EdgeInsets.symmetric(horizontal: 24.w),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            /// Search Field
-            CustomTextField(
+    return DefaultTabController(
+      length: tabbarList.length, // Ensuring TabController knows the correct number of tabs
+      child: Scaffold(
+        appBar: AppBar(
+          leading: InkWell(
+            onTap: () {
+              Get.back();
+            },
+            child: Icon(Icons.arrow_back_ios),
+          ),
+          backgroundColor: Colors.transparent,
+        ),
+        body: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 24.w),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              /// Search Field
+              CustomTextField(
                 contentPaddingVertical: 15.h,
-                hintText: "search",
+                hintText: "search".tr,
                 prefixIcon: Padding(
-                  padding:  EdgeInsets.symmetric(horizontal: 10.w),
-                  child: SvgPicture.asset(AppIcons.searchIcon,height: 24.h,width: 24.w,),
+                  padding: EdgeInsets.symmetric(horizontal: 10.w),
+                  child: SvgPicture.asset(
+                    AppIcons.searchIcon,
+                    height: 24.h,
+                    width: 24.w,
+                  ),
                 ),
-                controller: _searchCtrl),
-            SizedBox(height:16.h,),
-            Text(AppString.suggestedText,style: AppStyles.h4(family: "Schuyler"),),
+                controller: _searchCtrl,
+              ),
+              SizedBox(height: 16.h),
 
-            /// Request Cart List
-            ListView.builder(
-                itemCount: 5,
-                padding: EdgeInsets.symmetric(vertical:10.h),
-                shrinkWrap: true,
-                primary: false,
-                itemBuilder: (context,index){
-              return   Padding(
-                padding: EdgeInsets.symmetric(vertical:10.h),
-                child: FriendRequestCard(),
-              );
-            })
+              /// Tab Bar
+              TabBar(
+                indicatorColor: AppColors.primaryColor,
+                dividerColor: Colors.transparent,
+                padding: EdgeInsets.zero,
+                labelStyle: AppStyles.customSize(
+                  size: 16,
+                  fontWeight: FontWeight.w500,
+                  color: AppColors.textColor,
+                ),
+                unselectedLabelStyle: AppStyles.customSize(
+                  size: 14,
+                  fontWeight: FontWeight.w500,
+                  color: AppColors.subTextColor,
+                ),
+                tabs: [
+                  for (String tab in tabbarList) Tab(text: tab.tr),
+                ],
+              ),
+              SizedBox(height: 16.h),
 
+              /// Tab Bar View
+              Expanded(
+                child: TabBarView(
+                  children: [
+                    FriendSuggested(),
+                    GroupSuggested(),
+                  ],
+                ),
+              ),
 
-
-
-            
-          ],
+            ],
+          ),
         ),
       ),
     );
