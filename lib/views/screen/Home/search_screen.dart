45c16936aeac25ff6d818622732404b29eba4c49
alfayej55 <mdalfayej017fa@gmail.com>
2024-09-11 15:35:59 +0600

    search scren

---
+++ lib/views/screen/Home/search_screen.dart
@@ -0,0 +1,15 @@
+
+import 'package:flutter/material.dart';
+
+class SearchScreen extends StatelessWidget {
+  const SearchScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: Icon(Icons.arrow_back_ios),
+      ),
+    );
+  }
+}
