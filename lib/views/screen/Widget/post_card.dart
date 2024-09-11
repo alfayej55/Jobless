ad35981c554aaa96be1b889e0df5e7a1308256d1
alfayej55 <mdalfayej017fa@gmail.com>
2024-09-11 11:27:40 +0600

    network image add

--- lib/views/screen/Widget/post_card.dart
+++ lib/views/screen/Widget/post_card.dart
@@ -44,21 +44,20 @@ class PostCart extends StatelessWidget {
 
               ],
             ),
+          ),
+
+          /// Post Image and post Text Section
+          Column(
+            children: [
+              Text("A car wash is a facility or location where vehicles are cleaned and maintained, There are various types of car washes, each offering different levels of service and automation",
+                overflow: TextOverflow.ellipsis,
+                maxLines: 26,
+                textAlign:TextAlign.justify,
+                style: AppStyles.h5(color: AppColors.subTextColor),),
+              
+            ],
           )
 
-          // Row(
-          //   children: [
-          //     Container(
-          //       height:48.h,
-          //       width: 48.w,
-          //       decoration: BoxDecoration(
-          //         shape: BoxShape.circle,
-          //         image: DecorationImage(image: AssetImage(AppImage.personImage)),
-          //       ),
-          //     ),
-          //    
-          //   ],
-          // )
         ],
       ),
     );
