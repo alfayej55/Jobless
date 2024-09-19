01aed4b734a0e5cdda5a9646d5851d97a5a6e4ab
alfayej55 <mdalfayej017fa@gmail.com>
2024-09-19 10:14:26 +0600

    Group search done

--- lib/views/screen/Home/group_suggested.dart
+++ lib/views/screen/Home/group_suggested.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:jobless/utils/app_colors.dart';
+import 'package:jobless/utils/app_string.dart';
 
+import '../../../utils/style.dart';
+import '../../base/casess_network_image.dart';
+import '../../base/custom_button.dart';
+import '../../base/custom_outlinebutton.dart';
 import '../Widget/friend_request_card.dart';
 
 class GroupSuggested extends StatelessWidget {
@@ -16,7 +22,42 @@ class GroupSuggested extends StatelessWidget {
         itemBuilder: (context,index){
           return   Padding(
             padding: EdgeInsets.symmetric(vertical:10.h),
-            child: FriendRequestCard(),
+            child: Container(
+              child: Row(
+                children: [
+                  CustomNetworkImage(
+                    imageUrl: "https://www.befunky.com/images/prismic/82e0e255-17f9-41e0-85f1-210163b0ea34_hero-blur-image-3.jpg?auto=avif,webp&format=jpg&width=896",
+                    height: 64.h,
+                    width: 64.w,
+                    borderRadius: BorderRadius.circular(10.r),
+                  ),
+                  Expanded(
+                    child: Padding(
+                      padding:  EdgeInsets.only(left: 10.w),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                    
+                          Text("Labu Basar",style: AppStyles.customSize(size:14,fontWeight: FontWeight.w500,family: "Schuyler",),),
+                          Text("Here are some ideas for creating content about being jobless",
+                            overflow: TextOverflow.ellipsis,
+                            maxLines: 3,
+                            style: AppStyles.customSize(size:12,fontWeight: FontWeight.w400,),),
+                          SizedBox(height: 10.h,),
+                          CustomButton(
+                            height: 40.h,
+                              width: 217.w,
+                              textStyle: AppStyles.h4(family: "Schuyler",color: Colors.white),
+                              onTap: (){}, text:"Join Group")
+                    
+                    
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
           );
         });
   }
